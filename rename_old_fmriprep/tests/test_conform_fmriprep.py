d0f0921a026995bf637fb06e71ad0c6837fd34ee
James Kent <james-kent@uiowa.edu>
2019-11-07 00:57:46 -0600

    add test for gen_ref functionality

--- rename_old_fmriprep/tests/test_conform_fmriprep.py
+++ rename_old_fmriprep/tests/test_conform_fmriprep.py
@@ -1,11 +1,44 @@
 import os
 import json
+
+import requests
+import pytest
 from ..conform_fmriprep import rename_fmriprep_files
+
+
+@pytest.fixture
+def bold_file(request, tmp_path):
+    fname = ("sub-01_ses-post_task-flanker_bold_space-MNI152NLin2009cAsym"
+             "_variant-smoothAROMAnonaggr_preproc.nii.gz")
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    cache_file = os.path.join(cwd, "data", "cache.txt")
+    fname_full = os.path.join(cwd, "data", "dset2", fname)
+    bold_file = request.config.cache.get(cache_file, None)
+    if bold_file is None:
+        url = "https://osf.io/wtzde/download"
+        bold_file_request = requests.get(url, allow_redirects=True)
+        open(fname_full, 'wb').write(bold_file_request.content)
+        request.config.cache.set(cache_file, fname_full)
+
+    return fname_full
+
+
+def test_gen_ref(bold_file, tmp_path):
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    fmriprep_dir = os.path.join(cwd, 'data', 'dset2')
+    renamed_dir = tmp_path
+    dset_desc = os.path.join(fmriprep_dir, "dataset_description.json")
+    tst_json = rename_fmriprep_files(fmriprep_dir, str(renamed_dir), dset_desc, gen_ref=True)
+
+    with open(tst_json, 'r') as js2:
+        tst_dict = json.load(js2)
+ 
+    assert [k for k in tst_dict.keys() if "boldref" in k]
 
 
 def test_rename_fmriprep_files(tmp_path):
     cwd = os.path.dirname(os.path.realpath(__file__))
-    fmriprep_dir = os.path.join(cwd, 'data')
+    fmriprep_dir = os.path.join(cwd, 'data', 'dset1')
     renamed_dir = tmp_path
     dset_desc = os.path.join(fmriprep_dir, "dataset_description.json")
     # reference output
